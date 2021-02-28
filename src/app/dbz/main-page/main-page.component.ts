f0247e6f936dcd1794ce78a3df33e60592c364e2
andrescanm <andres.co@hotmail.com>
2021-02-27 21:11:10 -0500

    preventDefault Form/Submit (mala práctica)

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ClassGetter } from '@angular/compiler/src/output/output_ast';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainPageComponent{
+  agregar( event:any ): void{
+    event.preventDefault();
+    console.log("OK!!!");
   }
-
 }
