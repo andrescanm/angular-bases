473108cddf7793e0d38ddb9f3a419e1da8c64482
andrescanm <andres.co@hotmail.com>
2021-02-27 21:30:05 -0500

    ngModel

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,22 @@
 import { ClassGetter } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
 
+interface Personaje{
+  nombre: string,
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent{
+  nuevo: Personaje = {
+    nombre: 'Trunks',
+    poder: 0
+  }
+
   agregar(): void{
-    console.log("TEST - OK!!!");
+    console.log( this.nuevo );
   }
 }
